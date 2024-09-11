9ad021aae3c13ba4b93d80b38413cc177b96a86b
Jonah Lund <jonahlund@proton.me>
2024-09-11 17:52:21 +0200

    Change exported paths

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
+#![allow(clippy::new_without_default)]
+
 pub mod ast;
+
+mod parse;
+pub use parse::Parser;
+
 #[cfg(feature = "fmt")]
-pub mod fmt;
-pub mod parse;
+mod fmt;
+#[cfg(feature = "fmt")]
+pub use fmt::Formatter;
